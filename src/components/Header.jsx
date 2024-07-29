0764bb7b5472e76f4b1d86ab96da234a19300912
Sakib <sakibulalam923@gmail.com>
2024-07-29 15:45:06 +0600

    Added Header React icon library

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,48 @@
 import React from "react";
+import logo from "./../assets/Images/logo.png";
+import HeaderItem from "./HeaderItem";
+import {HiMagnifyingGlass,
+    HiHome,
+    HiStar,HiPlayCircle,
+    HiTv
+} from "react-icons/hi2";
+import {HiPlus, HiDotsVertical} from "react-icons/hi";
+
 
 function Header(){
+    const menu = [
+        {
+            name: "Home",
+            icon: HiHome
+        },
+        {
+            name: "Search",
+            icon: HiMagnifyingGlass
+        },
+        {
+            name: "Watchlist",
+            icon: HiStar
+        },
+        {
+            name: "Originals",
+            icon: HiPlus
+        },
+        {
+            name: "Movies",
+            icon: HiPlayCircle
+        },
+        {
+            name: "Series",
+            icon: HiTv,
+        }
+    ]
     return (
-        <div>Header</div>
+        <div className="flex items-center gap-8">
+            <img src={logo} className="w-[80px] object-cover md:w-[115px]" />
+            {menu.map((item, index) => (
+                <HeaderItem key={index} Icon={item.icon} title={item.name} />
+            ))}        
+        </div>
     )
 }
 
