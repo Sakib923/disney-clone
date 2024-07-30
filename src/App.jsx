efecb2c58f264ed7923b95e546f59f7d4e8b9824
Sakib <sakibulalam923@gmail.com>
2024-07-31 01:27:00 +0600

    All changes added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Headers from './components/Header'
 import Slider from './components/Slider'
 import ProductionHouse from './components/ProductionHouse'
+import GenreMovieList from './components/GenreMovieList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Headers />
       <Slider />
       <ProductionHouse />
+      <GenreMovieList />
     </div>
   )
 }
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -6,6 +6,7 @@ const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2
 
 const getTrendingVideos=axios.get(movieBaseUrl+
     "/trending/all/day?api_key="+api_key);
+
     const getMovieByGenreId=(id)=>
     axios.get(movieByGenreBaseURL+"&with_genres="+id)
 
